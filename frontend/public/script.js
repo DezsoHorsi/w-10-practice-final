f61eba7510e94c7305fe0c9e108e015d7f38fbb1
Dezső Horsi <dezso.horsi@gmail.com>
2023-04-11 11:49:12 +0200

    user post on webpage

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,45 +1,65 @@
 console.log('loaded')
 
-fetch(`http://127.0.0.1:9000/data`) // fontos, hogy itt az endpoint url-t adjuk meg
-.then(response => {
+const readingBeers = () => {
+  fetch('http://127.0.0.1:9000/data')
+.then(response => { 
   console.log(response)
-  if (response.status === 201) {
-    console.log('ok')
+  if (response.status === 200) {
+    console.log('OK')
   }
   return response.json()
 })
 .then(responseJson => {
-  console.log(responseJson)
   const data = responseJson
+  let html = ""
   data.forEach(element => {
-    console.log(element.name)
-    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
+    html = html + `
+    <h3>${element.name}</h3>
+    <span><strong>Abv.: </strong> ${element.abv}</span>
+    <span><strong>Brewery: </strong>${element.brewery}</span>
+    `
   });
+  beerListElement.innerHTML = html
 })
+} 
+readingBeers();
 
-
-// UPLOAD DATA AND FILES 
 const formComponent = () => `
-  <form>
+  <form id="upload-file">
     <input type="text" placeholder="Image name" name="name">
     <input type="file" name="file">
     <button>SEND</button>
   </form>
 `
+const newBeerComponent = () => `
+  <form id="add-beer">
+    <input type="text" name="beerName" placeholder="Beer name">
+    <input type="text" name="abv" placeholder="beer abv">
+    <input type="text" name="brewery" placeholder="brewery">
+    <button>Add new</button>
+  </form>
+`
+
+
+
 
 const rootElement = document.querySelector('#root')
 
 rootElement.insertAdjacentHTML('beforeend', formComponent())
+rootElement.insertAdjacentHTML('beforeend', newBeerComponent())
+rootElement.insertAdjacentHTML('beforeend', '<div id="beer-list"></div>')
 
-const formElement = document.querySelector('form')
+const beerListElement = document.querySelector('#beer-list')
 
-formElement.addEventListener('submit', (event)=> {
+const formElement = document.querySelector('#upload-file')
+
+formElement.addEventListener('submit', (event) => {
   event.preventDefault()
-  console.log("Submitted")
+  console.log('submit')
 
   const formData = new FormData()
-  formData.append('name', document.querySelector(`input[type='text']`).value)//!!! Attribútum alapján kiszelektálni elemeket
-  formData.append('image', document.querySelector(`input[type='file']`).files[0]) // ez mindig egy tömb lesz meg ha egy eleme van is
+  formData.append('name', document.querySelector(`input[type='text']`).value)
+  formData.append('image', document.querySelector(`input[type='file']`).files[0])
 
   fetch('/upload', {
     method: 'POST',
@@ -48,15 +68,39 @@ formElement.addEventListener('submit', (event)=> {
   .then(res => {
     console.log("res", res)
     if(res.status === 200) {
-      console.log('Success!')
-      return res.json()
-    }else {
+      console.log('success!')
+      return res.json() // return  "pelda" itt szedjük ki az értéket, itt adjuk tovább a köv.then-nek
+    } else {
       console.log("ERROR!")
     }
-  }) // kicsomagoljuk
-  .then(resData => { // resData = "pelda"
-    console.log(resData)
+  })
+  .then(resData => { // ebben a callback fn-ben resData néven fogadjuk az előző callback fn visszatérését: resData = "pelda". Így közlekedik az adatom a backend és frontend között.
     rootElement.insertAdjacentHTML('beforeend', `<img src="./public/${resData}.jpg">`)
   })
   .catch(error => console.log(error))
+})
+
+const addNewFormElement = document.querySelector('#add-beer')
+addNewFormElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const product = {
+    name: document.querySelector('input[name="beerName"]').value,
+    abv: document.querySelector('input[name="abv"]').value,
+    brewery: document.querySelector('input[name="brewery"]').value
+  }
+
+  //console.log('Ez itt a product: ', product)
+  
+  fetch('/new', {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    }, 
+    body: JSON.stringify(product)
+  })
+  .then(res => {
+    readingBeers();
+    addNewFormElement.reset();
+  })
 })
